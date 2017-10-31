0b374a332f7a81658409f404d2b6909eac20aabe
Eric Sullivan <eric.sullivan@annkissam.com>
2017-10-31 14:08:43 -0400

    bundle gem resque-batch

---
+++ lib/resque/batch.rb
@@ -0,0 +1,7 @@
+require "resque/batch/version"
+
+module Resque
+  module Batch
+    # Your code goes here...
+  end
+end
---
+++ lib/resque/batch/version.rb
@@ -0,0 +1,5 @@
+module Resque
+  module Batch
+    VERSION = "0.1.0"
+  end
+end
